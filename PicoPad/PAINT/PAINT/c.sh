831f24eea369b8757c9b668bce85505ff3224e3d
Damian V. Čechov <120730543+DamianVCechov@users.noreply.github.com>
2023-09-26 02:19:31 +0200

    ver. 1.00

---
+++ PicoPad/PAINT/PAINT/c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Compilation...
+
+export TARGET=PAINT
+export GRPDIR=PAINT
+
+../../../_c1.sh "$1"
---
+++ PicoPad/PAINT/PAINT/e.bat
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Export to hardware...
+
+export TARGET="PAINT"
+../../../_e1.sh
